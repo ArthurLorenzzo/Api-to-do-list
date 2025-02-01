b17c5a5b3cd66a93c505df54765a50466572bdf6
ArthurLorenzzo <arthurlbo11@gmail.com>
2025-02-01 12:58:37 -0300

    first commit

---
+++ src/test/java/com/br/arthur/apitodolist/ApiToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.arthur.apitodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
